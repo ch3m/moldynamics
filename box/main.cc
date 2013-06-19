138c30fe66cba1f062b13957bf0930c3bce84e28
ch3m <josema_zamora@lufac.com>
2013-06-19 17:15:55 -0500

    se agregan ejemplos del template

---
+++ box/main.cc
@@ -0,0 +1,13 @@
+#include <cstdio>  
+#include <cstdlib>
+#include <iostream>
+#include "Box.h"
+
+using namespace std;
+
+// main routine that executes on the host  
+int main(void){  
+  //SimParams Params; //Create object with parameters simulation(Steps,average...)
+  Box<float> boxset;
+  return 0;
+}
